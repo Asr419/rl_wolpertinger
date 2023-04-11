8676f360d658d68a70553d2b620913e06fdfd0c4
damicoedoardo <damicoedoardo95@gmail.com>
2023-04-11 22:43:44 +0100

    fixed bug intent

--- src/rl_recsys/user_modeling/user_state.py
+++ src/rl_recsys/user_modeling/user_state.py
@@ -33,43 +33,67 @@ class AbstractUserState(nn.Module, metaclass=abc.ABCMeta):
 class AlphaIntentUserState(AbstractUserState):
     def __init__(self, user_features: torch.Tensor, **kwds: Any) -> None:
         super().__init__(**kwds)
-        # called p_u in the paper
         self.user_features = user_features
 
-        # assure tgt feature is positive
+        # select a target feature for the user among the positve ones
         tgt_feature_idx = None
         tgt_feat_val = -1
         while tgt_feat_val < 0:
             tgt_feature_idx = torch.randint(0, len(user_features), size=(1,))
             tgt_feat_val = user_features[tgt_feature_idx]
-
         self.tgt_feature_idx = tgt_feature_idx
-        # called p_uh in the paper
+
         user_state = self.generate_state(self.user_features)
-        self.user_state_init = user_state.clone()
         self.register_buffer("user_state", user_state)
+        # used to reset the intent to the initial create one at the end of an episode
+        self.register_buffer("user_state_init", user_state)
 
-    def generate_state(self, user_features: torch.Tensor) -> torch.Tensor:
-        user_state = torch.Tensor(user_features).clone()
+    # def generate_state(self, user_features: torch.Tensor) -> torch.Tensor:
+    #     user_state = torch.Tensor(user_features).clone()
+    #     # sample alpha from a uniform distribution
+    #     alpha = torch.rand(1)
+    #     # alpha = 0.8
+    #     alpha = 0.8 * alpha + 0.2  # alpha between 0.2 and 1
+
+    #     inv_alpha = 1 - alpha
+
+    #     # creating tgt feature mask and inverse mask
+    #     feat_mask = torch.zeros(len(user_state))
+    #     inv_feat_mask = torch.ones(len(user_state))
+    #     # select target feature randomly
+    #     feat_mask[self.tgt_feature_idx] = 1
+    #     inv_feat_mask[self.tgt_feature_idx] = 0
+
+    #     user_state[feat_mask == 1] = alpha * user_state[feat_mask == 1]
+    #     user_state[inv_feat_mask == 1] = (
+    #         inv_alpha / (len(user_state) - 1) * user_state[inv_feat_mask == 1]
+    #     )
+
+    #     return user_state
+    def generate_state(self, user_features: npt.NDArray[np.float_]) -> torch.Tensor:
+        user_state = user_features.copy()
+
+        tgt_feature_idx = None
+        tgt_feat_val = -1
+        while tgt_feat_val < 0:
+            tgt_feature_idx = np.random.randint(0, len(user_features), size=(1,))
+            tgt_feat_val = user_features[tgt_feature_idx]
+
         # sample alpha from a uniform distribution
-        alpha = torch.rand(1)
-        # alpha = 0.8
-        alpha = 0.8 * alpha + 0.2  # alpha between 0.2 and 1
+        alpha = np.random.uniform()
+        print(alpha)
+        print(tgt_feature_idx)
 
-        inv_alpha = 1 - alpha
+        pos_sum = np.sum(user_state[user_state > 0])
+        pos_val_count = len(user_state[user_state > 0])
 
-        # creating tgt feature mask and inverse mask
-        feat_mask = torch.zeros(len(user_state))
-        inv_feat_mask = torch.ones(len(user_state))
-        # select target feature randomly
-        feat_mask[self.tgt_feature_idx] = 1
-        inv_feat_mask[self.tgt_feature_idx] = 0
+        user_state[tgt_feature_idx] = user_state[tgt_feature_idx] + (2 * alpha)
+        user_state[user_state > 0] -= alpha / pos_val_count
 
-        user_state[feat_mask == 1] = alpha * user_state[feat_mask == 1]
-        user_state[inv_feat_mask == 1] = (
-            inv_alpha / (len(user_state) - 1) * user_state[inv_feat_mask == 1]
-        )
+        if user_state[tgt_feature_idx] > 1:
+            user_state[user_state > 0] /= user_state[tgt_feature_idx]
 
+        user_state = torch.Tensor(user_state)
         return user_state
 
     def reset_state(self) -> None:
