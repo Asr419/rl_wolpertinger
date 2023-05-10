143178b40af629acfc31bcb3703fce0e4aabef5b
damicoedoardo <damicoedoardo95@gmail.com>
2023-05-10 09:15:25 +0100

    delete unused files

--- tests/user_modeling/test_user_features_gen.py
+++
@@ -1,9 +0,0 @@
-from rl_recsys.user_modeling.features_gen import NormalUserFeaturesGenerator
-
-
-def test_normal_user_features_gen():
-    """Test normal user features generator"""
-    num_features = 5
-    user_features_gen = NormalUserFeaturesGenerator()
-    user_features = user_features_gen(num_features)
-    assert user_features.shape == (num_features,)
